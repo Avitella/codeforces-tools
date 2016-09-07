51d4ca7cd2c91327fc432b9d1339fc3055b2e835
vagrant <vagrant@vagrant.vm>
2016-09-07 23:06:44 +0000

    removing scipy from build

--- libs/ml/__init__.py
+++ libs/ml/__init__.py
@@ -1,5 +1,6 @@
-import scipy.stats
+#import scipy.stats
 
 def likes_vs_dislikes_rank(likes, dislikes):
-    THRESHOLD = 0.95
-    return scipy.stats.beta.ppf(THRESHOLD, likes + 1, dislikes + 1)
+#    THRESHOLD = 0.95
+#    return scipy.stats.beta.ppf(THRESHOLD, likes + 1, dislikes + 1)
+    return (likes + 1) * 1.0 / (dislikes + 1)
