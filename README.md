52b84d5100ae62759fef36710caf5cf40ac7098d
bdzl <vladb15@mail.ru>
2016-09-07 23:20:51 +0000

    xif

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 ## codeforces-tagstat example
 * Install https://www.bazel.io (just `brew install bazel` on osx)
-* ``building: ./build.sh
-* ``running: ./run.sh <arguments>
+* Build: ./build.sh
+* Run: ./run.sh <arguments>
--- libs/ml/__init__.py
+++ libs/ml/__init__.py
@@ -3,4 +3,4 @@
 def likes_vs_dislikes_rank(likes, dislikes):
 #    THRESHOLD = 0.95
 #    return scipy.stats.beta.ppf(THRESHOLD, likes + 1, dislikes + 1)
-    return (likes + 1) * 1.0 / (dislikes + 1)
+    return (likes + 1) * 1.0 / (dislikes + likes + 1)
